4bc0fe3bd2ac78d3f891ac62c42005909ba890cf
Daniel Tomsu <daniel@tomsu.de>
2016-07-18 05:20:14 +0200

    added spring boot application

---
+++ gs-consuming-rest/src/main/java/de/tomsu/spring/model/Quote.java
@@ -0,0 +1,37 @@
+package de.tomsu.spring.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * The model for a Quote.
+ * Created by Daniel on 18.07.2016.
+ */
+@JsonIgnoreProperties(ignoreUnknown = false)
+public class Quote {
+    private String type;
+    private Value value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
