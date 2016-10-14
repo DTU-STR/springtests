0a207babd6c2350e35425d2ccca977f13c98ae78
Daniel <Daniel@myPC.local>
2016-10-14 19:00:46 +0200

    initial commit after maven create from archetype

---
+++ gs-webservice-soap/src/main/java/de/tomsu/springtests/App.java
@@ -0,0 +1,13 @@
+package de.tomsu.springtests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
