04fbcf7317df155ac9ec33e6427c1f1b2c399855
DTU-travelpals <daniel.tomsu@travelpals.de>
2016-10-15 14:08:32 +0200

    initial commit of gs-integration

---
+++ gs-integration/src/main/java/de/tomsu/springtests/GsIntegrationApplication.java
@@ -0,0 +1,12 @@
+package de.tomsu.springtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsIntegrationApplication.class, args);
+	}
+}
